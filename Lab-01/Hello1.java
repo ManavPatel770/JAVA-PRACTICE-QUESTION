c48253a3f64f9a2e67f02d980231b826d77848cc
manav <mkpatel8571@gmail.com>
2025-09-10 22:00:55 +0530

    github demo - 2

---
+++ Lab-01/Hello1.java
@@ -0,0 +1,5 @@
+public class Hello1{
+    public start void main(String[] args){
+        System.out.println("Hello Lab-01......");
+    }
+}
---
+++ Lab-02/Hello2.java
@@ -0,0 +1,5 @@
+public class Hello2{
+    public start void main(String[] args){
+        System.out.println("Hello Lab-02......");
+    }
+}
